3ede81646a702490e16f351b3b9f5d60e847f0e9
rammyres <rammyres@gmail.com>
2021-01-25 21:35:28 -0300

    Alteração para KivyMD

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-import kivy
-from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+#!/usr/bin/env python3
 
-    
-class RDVEColetaApp(App):
-       
+import kivy
+from kivymd.app import MDApp
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class TelaColeta(Screen):
+    pass
+
+# class TelaQREleitor(Screen):
+#     pass
+# class TesteLeitor(Screen):
+#     pass
+
+
+class RDVEColetaApp(MDApp):
+           
     def build(self):
-        return FloatLayout()
+        self.theme_cls.primary_palette = "BlueGray"
+        gerenciadorTela = ScreenManager()
+        gerenciadorTela.add_widget(TelaColeta(name='TelaInicial'))
+        # gerenciadorTela.add_widget(TesteLeitor(name='Teste'))
+        # gerenciadorTela.add_widget(TelaQREleitor(name='TelaQREleitor'))
+
+        gerenciadorTela.current='TelaInicial'
+        return TelaColeta()
         
 
 if __name__ == "__main__":
